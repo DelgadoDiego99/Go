bf75a5d052f6eb373a56ef64a089cd6c855f0455
DDelgadoT <delgadodie99@gmail.com>
2022-03-19 18:32:20 -0500

    Ultimos cambios

--- src/diccionarios.go
+++ src/diccionarios.go
@@ -18,6 +18,17 @@ func main() {
 	// Encontrar valor
 	value, ok := m["Jose"] // Existe la llave
 	fmt.Println(value, ok)
-	value2, ok2 := m["Josep"] // No existe la llave por lo que se imprime 9
+	value2, ok2 := m["Josep"] // No existe la llave por lo que se imprime 0
 	fmt.Println(value2, ok2)
+
+	// Maps con varios valores para una sola key
+	m2 := make(map[int][]string)
+	listado2020 := []string{"Jose", "Pedro", "Juan"}
+	m2[2020] = listado2020
+	fmt.Println(m2)
+
+	// Recorrer map que contiene multiple valores
+	for i, valor := range m2 {
+		fmt.Println(i, valor)
+	}
 }
